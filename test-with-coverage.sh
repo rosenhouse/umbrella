f75f1c105cfeee9be35dab48db78324ab5ce838c
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-11-12 23:46:41 -0800

    CI fixups

--- test-with-coverage.sh
+++ test-with-coverage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e -u
+set -o pipefail
 
 ginkgo -r -p --randomizeAllSpecs --randomizeSuites --failOnPending \
     --trace --race --compilers=2 \
     -coverpkg=github.com/rosenhouse/umbrella/... \
-    -covermode=set -coverprofile=umbrella.coverprofile
+    -covermode=atomic -coverprofile=umbrella.coverprofile
 
 # find . -name "*.coverprofile*" | xargs gocovmerge > all.coverprofile
